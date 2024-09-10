a8806360a028c2afccc3a4178bdd12d3b8cff1ec
putte <putte@PLATON>
2024-09-10 16:00:38 +0200

    Add project files.

---
+++ Personalregister/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personalregister
+{
+    //Class of employers with the attributes names and wages.
+    internal class Employee
+    {
+        public string Name {  get; set; }
+        public double Wage { get; set; }
+
+        //Constructor
+        public Employee(string name, double salary) 
+        {
+            this.Name = name;
+            this.Wage = salary;
+        }
+    }
+}
---
+++ Personalregister/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+
+namespace Personalregister
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create empty list of employees
+            var employees = new List<Employee>();
+            
+            string answer = "j";
+
+            //Add employees
+            while (answer == "j")
+            {
+                Console.Write("Namn: ");
+                string name = Console.ReadLine()!;
+                Console.Write("Lön: ");
+                double salary = Convert.ToDouble(Console.ReadLine());
+                Employee empl = new(name, salary);
+                employees.Add(empl); 
+                Console.WriteLine("Om du vill fortsätta svara j annars svara n.");
+                answer = Console.ReadLine()!;
+            }
+            
+            //Print the employee register
+            Console.WriteLine("\nNamn\t\tLön");
+            foreach (Employee emp in employees)
+            {
+                Console.WriteLine($"{emp.Name}\t\t{emp.Wage}");
+            }
+        }
+    }
+}
